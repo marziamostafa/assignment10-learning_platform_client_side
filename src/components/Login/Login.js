dd1d23a15623217881f14bd8c5786a792e9d9e50
marziamostafa <marziamostafa@gmail.com>
2022-10-25 14:43:35 +0600

    fifth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FaGoogle, FaGithub } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
 
 const Login = () => {
     return (
-        <div>
-            <h2>This Is log in page</h2>
+        <div className='mt-4'>
+            <h2 className='text-center mb-4'>How do you want to log In?</h2>
+            <ButtonGroup vertical className='form-control '>
+                <Button className='mb-2' variant="outline-dark"><AiOutlineMail></AiOutlineMail> Log in With Email & Password</Button>
+                <Button className='mb-2' variant="outline-dark"><FaGoogle></FaGoogle> Log in with Google</Button>
+                <Button variant="outline-dark"><FaGithub></FaGithub> Log in with GitHub</Button>
+            </ButtonGroup>
+
         </div>
+
     );
 };
 
